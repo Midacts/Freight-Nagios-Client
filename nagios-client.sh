e27af4955b5adf01990b953b7b7309852bcc2df0
John McCarthy <midactsmystery@gmail.com>
2014-05-25 23:14:08 -0400

    Update nagios-client.sh

--- nagios-client.sh
+++ nagios-client.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Freight Repo Setup Script
-# Date: 20th of May, 2014
+# FPM Packaging with Freight Hosting Script for Nagios Clients
+# Date: 25th of May, 2014
 # Version 1.0
 #
 # Author: John McCarthy
@@ -10,44 +10,169 @@
 # To God only wise, be glory through Jesus Christ forever. Amen.
 # Romans 16:27, I Corinthians 15:1-4
 #---------------------------------------------------------------
-function sources.list.d(){
-	# Gets the address to reach the FPM/Frieght host by
+######## VARIABLES ########
+plugin_version=2.0.1
+nrpe_version=2.15
+function nagios-plugins(){
+# Install the prerequisite packages for Nagios Plugins
 		echo
-		echo -e '\e[33mWhat is the best way for machines to reach Your Freight host ?\e[0m'
-		echo -e '\e[30;01mMake sure machines can ping the Freight machine by this address\e[0m'
-		read address
-
-	# Creating the sources.list.d file and GPG file
-		echo
-		echo -e '\e[33mWhat would you like to name your sources.list.d file and GPG file ?\e[0m'
-		echo -e '\e[33;01mFor Example: Type "example" to get example.list and example.gpg as filenames\e[0m'
-		read filename
-		echo "deb http://$address $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/$filename.list
-
-	# Downloads the GPG file
-		echo
-		echo -e '\e[01;34m+++ Downloading your freight GPG key...\e[0m'
-		wget -O /etc/apt/trusted.gpg.d/$filename.gpg http://$address/keyring.gpg
-
-	# Update sources list
+		echo -e '\e[01;34m+++ Installing the prerequisite software...\e[0m'
 		apt-get update
-		echo -e '\e[01;37;42mYour freight GPG key has been successfully downloaded!\e[0m'
+		apt-get install -y build-essential libgd2-xpm-dev libssl-dev
+		echo -e '\e[01;37;42mThe prerequisite software has been successfully installed!\e[0m'
+
+	# Downloaded the latest Nagios Plugins files
+		echo
+		echo -e '\e[01;34m+++ Downloading the latest Nagios Plugins files...\e[0m'
+		cd
+		wget https://www.nagios-plugins.org/download/nagios-plugins-$plugin_version.tar.gz
+
+	# Extract the NRPE Plugin
+		tar xzf nagios-plugins-$plugin_version.tar.gz
+		cd nagios-plugins-$plugin_version
+		echo -e '\e[01;37;42mThe latest Nagios Plugins files have been successfully downloaded!\e[0m'
+
+	# Configure the installation
+		echo
+		echo -e '\e[01;34m+++ Configuring the Nagios Plugins installation files...\e[0m'
+		useradd nagios
+		./configure --with-nagios-user=nagios --with-nagios-group=nagios --with-openssl=/usr/bin/openssl --enable-perl-modules --enable-libtap
+		make
+
+	# Create the directories to house the installation files
+		mkdir /tmp/installdir/nagios-plugins
+
+	# Makes the installation file
+		make install DESTDIR=/tmp/installdir/nagios-plugins
+		echo -e '\e[01;37;42mThe Nagios Plugin installation files have been configured!\e[0m'
+
+	# Create the --after-installation script for Nagios Plugins
+		cat << 'EOP' > /root/plugins.sh
+#!/bin/bash
+	# Set Folder Permissions
+		useradd nagios
+		chown nagios:nagios /usr/local/nagios
+		chown -R nagios:nagios /usr/local/nagios/libexec
+
+EOP
+
+	# Use FPM to make the .deb Nagios Plugins package
+		echo
+		echo -e '\e[01;34m+++ Creating the Nagios Plugins package...\e[0m'
+		echo
+		fpm -s dir -t deb -n nagios-plugins-client -v $plugin_version -d "nrpe-client (>= 2.15)" -d "libssl-dev (>= 1.0.1e-2+deb7u7)" --after-install /root/plugins.sh -C /tmp/installdir/nagios-plugins usr
+
+	# Move the Nagios Plugins package to the root directory
+		mv nagios-plugins-client_"$plugin_version"_amd64.deb /root
+		echo
+		echo -e '\e[01;37;42mThe Nagios Plugins package has been successfully created!\e[0m'
 }
-function doAll()
-{
-	# Calls Function 'sources.list.d'
-		echo -e "\e[33m=== Add your Freight Repo to your sources.list.d directory ? (y/n)\e[0m"
+function nrpe(){
+	# Download the latest nrpe files
+		echo
+		echo -e '\e[01;34m+++ Downloading the latest nrpe files...\e[0m'
+		cd
+		wget http://sourceforge.net/projects/nagios/files/nrpe-$nrpe_version.tar.gz
+
+	# Untar the nrpe files
+		tar xzf nrpe-$nrpe_version.tar.gz
+		cd nrpe-$nrpe_version
+		echo -e '\e[01;37;42mThe latest nrpe files have been successfully downloaded!\e[0m'
+
+	# Configure the nrpe installation
+		echo
+		echo -e '\e[01;34m+++ Configuring the nrpe installation files...\e[0m'
+		./configure --with-ssl=/usr/bin/openssl --with-ssl-lib=/usr/lib/x86_64-linux-gnu
+		make
+		make all
+
+	# Create the directory to house the install
+		mkdir -p /tmp/installdir/nrpe/etc/init.d/
+
+	# Makes the installtion files
+		make install DESTDIR=/tmp/installdir/nrpe
+		make install-plugin DESTDIR=/tmp/installdir/nrpe
+		make install-daemon DESTDIR=/tmp/installdir/nrpe
+		make install-daemon-config DESTDIR=/tmp/installdir/nrpe
+
+	# Copies over the nrpe init scripts and makes it executable
+		cp init-script.debian /tmp/installdir/nrpe/etc/init.d/nrpe
+		chmod 700 /tmp/installdir/nrpe/etc/init.d/nrpe
+		echo -e '\e[01;37;42mThe nrpe installation files have been configured!\e[0m'
+
+	# Create the --after-installation script for nrpe
+		cat << 'EOR' > /root/nrpe.sh
+#!/bin/bash
+	# Starting nrpe at boot time
+		update-rc.d nrpe defaults
+
+EOR
+
+	# Use FPM to make the .deb nrpe package
+		echo
+		echo -e '\e[01;34m+++ Creating the nrpe package...\e[0m'
+		echo
+		fpm -s dir -t deb -n nrpe-client -v $nrpe_version -d "nagios-plugins-client (>=2.0.1)" -C /tmp/installdir/nrpe usr etc
+
+	# Move the Nagios Plugins package to the root directory
+		mv nrpe-client_"$nrpe_version"_amd64.deb /root
+		echo
+		echo -e '\e[01;37;42mThe nrpe package has been successfully created!\e[0m'
+
+}
+function freight(){
+	# Finds the Nagios Plugins package
+		plugins_file=$(find -name "nagios-plugins-client_*")
+		plugins=$(echo $plugins_file | awk '{$0=substr($0,3,length($0)); print $0}')
+
+	# Finds the nrpe package
+		nrpe_file=$(find -name "nrpe-client_$nrpe_version*")
+		nrpe=$(echo $nrpe_file | awk '{$0=substr($0,3,length($0)); print $0}')
+
+	# Adding your FPM packages to your freight repo
+		echo
+		echo -e '\e[33mWhat repo do you want to put these files in ?\e[0m'
+		echo -e '\e[31m  Please put a space beteen each repo\e[0m'
+		echo -e '\e[33;01mFor example: apt/squeeze apt/wheezy apt/trusty\e[0m'
+		read -ra repo
+		/usr/bin/freight add $plugins $nrpe ${repo[0]} ${repo[1]} ${repo[2]} ${repo[3]} ${repo[4]}
+		echo
+		echo -e '\e[30;01mPlease type in your GPG Key passphrase for as many repos you are adding\e[0m'
+		echo
+		/usr/bin/freight cache
+}
+function doAll(){
+	# Calls Function 'nagios-plugins'
+		echo
+		echo
+		echo -e "\e[33m=== Package Nagios Plugins ? (y/n)\e[0m"
 		read yesno
 		if [ "$yesno" = "y" ]; then
-			sources.list.d
+			nagios-plugins
+		fi
+
+	# Calls Function 'nrpe'
+		echo
+		echo -e "\e[33m=== Package nrpe ? (y/n)\e[0m"
+		read yesno
+		if [ "$yesno" = "y" ]; then
+			nrpe
+		fi
+
+	# Calls Function 'freight'
+		echo
+		echo -e "\e[33m=== Add these packages to your Freight repo ? (y/n)\e[0m"
+		read yesno
+		if [ "$yesno" = "y" ]; then
+			freight
 		fi
 
 	# End of Script Congratulations, Farewell and Additional Information
 		clear
-		FARE=$(cat << EOZ
+		farewell=$(cat << EOZ
 
 
-          \e[01;37;42mWell done! You have completed your Adding your Freight Repo! \e[0m
+\e[01;37;42mWell done! You have created your FPM package and hosted it on your Freight repo!\e[0m
 
   \e[30;01mCheckout similar material at midactstech.blogspot.com and github.com/Midacts\e[0m
 
@@ -58,25 +183,32 @@ EOZ
 )
 
 		#Calls the End of Script variable
-		echo -e "$FARE"
+		echo -e "$farewell"
 		echo
 		echo
 		exit 0
 }
 
-# Check privileges
-[ $(whoami) == "root" ] || die "You need to run this script as root."
+	# Check privileges
+		[ $(whoami) == "root" ] || die "You need to run this script as root."
 
-# Welcome to the script
-clear
-echo
-echo
-echo -e '               \e[01;37;42mWelcome to Midacts Mystery'\''s Freight Repo Script!\e[0m'
-echo
-echo
-case "$go" in
-        * )
-                        doAll ;;
-esac
+	# Welcome to the script
+		clear
+		welcome=$(cat << EOA
 
-exit 0
+
+     \e[01;37;42mWelcome to Midacts Mystery's FPM Packaging and Freight Hosting Script!\e[0m
+EOA
+)
+
+	# Calls the welcome variable
+		echo -e "$welcome"
+
+	# Calls the doAll function
+		case "$go" in
+			* )
+				doAll ;;
+		esac
+
+	# Exits the script
+		exit 0
